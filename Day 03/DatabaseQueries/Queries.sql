ed8876c5a40279d67480bdf7af24ab315e1630cd
Md Irfan <work.mdirfan@gmail.com>
2025-06-30 21:01:18 +0530

    Database Queries

---
+++ Day 03/DatabaseQueries/Queries.sql
@@ -0,0 +1,37 @@
+---------------------------**DATABASE QUERIES**---------------------------
+
+---1. CREATE DATABASE db_name;
+---2. CREATE DATABASE IF NOT EXISTS db_name;
+---3. DROP DATABASE db_name;
+---4. DROP DATABASE IF EXISTS db_name;
+---5. SHOW DATABASES;
+---6. SHOW TABLES;
+
+
+
+
+------------------------1. CREATE DATABASE db_name;-----------------------
+
+CREATE DATABASE school;
+
+------------------------2. CREATE DATABASE IF NOT EXISTS db_name;-----------------------
+
+CREATE DATABASE IF NOT EXISTS school;
+
+------------------------3. DROP DATABASE db_name;-----------------------
+
+DROP DATABASE school; 
+
+------------------------4. DROP DATABASE IF EXISTS db_name;-----------------------
+
+DROP DATABASE IF EXISTS school;
+
+------------------------5. SHOW DATABASES;-----------------------
+
+SHOW DATABASES;
+
+------------------------6. SHOW TABLES;-----------------------
+
+SHOW TABLES;
+
+
